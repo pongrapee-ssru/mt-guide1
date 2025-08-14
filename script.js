1edc69aed09b6e7aaec2f8f9c3862cea185a20eb
pongrapee-ssru <pongrapee.ka@ssru.ac.th>
2025-08-14 11:21:54 +0700

    Original

---
+++ script.js
@@ -0,0 +1,20 @@
+document.getElementById("volumeCalculator").addEventListener("submit", function(event) {
+   
+    // Prevent form submission
+    event.preventDefault(); 
+
+    // Get the input values
+    const radius = parseFloat(document.getElementById('inRadius').value);
+    const height = parseFloat(document.getElementById('inHeight').value);
+    const unit = parseFloat(document.getElementById("inUnit").value);
+    //console.log(radius, height, unit); // Debugging output
+
+    // Perform the calculation
+    const volume = Math.PI * Math.pow(radius, 2) * height / unit; // Perform V = PI * r^2 * h
+
+    // Build result message
+    const resultMessage = "Volume = " + volume.toFixed(2) + " cm<sup>3</sup>";
+
+    // Display result
+    document.getElementById("result").innerHTML = resultMessage;
+});  
