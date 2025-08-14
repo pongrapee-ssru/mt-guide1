12d27efeac8e3f5f71919f033f68a0dd97c3f91f
pongrapee-ssru <pongrapee.ka@ssru.ac.th>
2025-08-14 11:52:37 +0700

    Add disabled input

--- script.js
+++ script.js
@@ -4,17 +4,14 @@ document.getElementById("volumeCalculator").addEventListener("submit", function(
     event.preventDefault(); 
 
     // Get the input values
-    const radius = parseFloat(document.getElementById('inRadius').value);
-    const height = parseFloat(document.getElementById('inHeight').value);
+    const radius = parseFloat(document.getElementById("inRadius").value);
+    const height = parseFloat(document.getElementById("inHeight").value);
     const unit = parseFloat(document.getElementById("inUnit").value);
     //console.log(radius, height, unit); // Debugging output
 
     // Perform the calculation
     const volume = Math.PI * Math.pow(radius, 2) * height / unit; // Perform V = PI * r^2 * h
 
-    // Build result message
-    const resultMessage = "Volume = " + volume.toFixed(2) + " cm<sup>3</sup>";
-
-    // Display result
-    document.getElementById("result").innerHTML = resultMessage;
+    // Display result in disabled input
+    document.getElementById("result").value = volume.toFixed(2);
 });  
